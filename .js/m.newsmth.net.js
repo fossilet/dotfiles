69d141d49a957cb9e3043a0b48fa194ada42e1af
Yongzhi Pan <panyongzhi@gmail.com>
2015-06-29 14:49:36 +0800

    Add newsmth and zdic.

---
+++ .js/m.newsmth.net.js
@@ -0,0 +1,5 @@
+// Since Mon Jun 29 14:22:28 CST 2015
+$('body').css({
+    'margin': '0px auto',
+    'width': '800px'
+})
---
+++ .js/wap.zdic.net.js
@@ -0,0 +1,10 @@
+// Mon Jun 29 14:46:22 CST 2015
+
+$('body').css({
+    'width': '700px',
+    'margin': '0px auto'
+})
+
+$('header.sheader').css({
+    'margin-top': '4px'
+})
